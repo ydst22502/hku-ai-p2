a8936d6ebb1075208170f77ebbcca7293cc8197f
ydst22502 <turtle920@foxmail.com>
2016-04-03 20:35:05 +0800

    ttt complete

--- solveTicTacToe.py
+++ solveTicTacToe.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 class GameState():
 
@@ -17,12 +18,15 @@ class GameState():
 						['3','X','5'],
 						['X','7','8']]
 
-		self.playerIndex = 'AI'
+		self.playerIndex = 'HM'
 
+		"""
 		boardNum = random.choice(['A','B','C'])
 		pos = random.randint(0,8)
 		self.latestPosition = (boardNum, pos)
 		self.placeTo(boardNum, pos)
+		"""
+		self.latestPosition = ('None', 0)
 
 	def display(self):
 		#print self.playerIndex + ': ' + self.latestPosition[0] + str(self.latestPosition[1])
@@ -72,6 +76,22 @@ class GameState():
 				break
 		self.placeTo(boardNum, pos)
 		
+	def getLegalActions(self):
+		legalActions = []
+		for boardNum in ['A', 'B', 'C']:
+			if not self.isDead(boardNum):
+				for i in range(3):
+					for j in range(3):
+						if self.board[boardNum][i][j] != 'X':
+							legalActions.append((boardNum, i*3+j))
+		return legalActions
+
+	def generateSuccessor(self, action):
+		boardNum = action[0]
+		pos = int(action[1])
+		successorGameState = copy.deepcopy(self)
+		successorGameState.placeTo(boardNum, pos)
+		return successorGameState
 
 	def isLose(self):
 		if self.isDead('A') and self.isDead('B') and self.isDead('C'):
@@ -98,6 +118,215 @@ class GameState():
 			return True
 		return False
 
+class OneBoardState():
+
+	def __init__(self, board):
+		self.boardT = [['X','1','2'],
+						['3','4','5'],
+						['6','7','8']]
+		self.board = board
+		self.board = self.boardT
+		self.playerIndex = 0
+
+	def display(self):
+		for row in self.board:
+			print row
+
+	def placeTo(self, pos = 0):
+		self.board[pos/3][pos%3] = 'X'
+
+	def isLose(self):
+		if self.board[0][0] == 'X' and self.board[0][1] == 'X' and self.board[0][2] == 'X':
+			return True
+		if self.board[1][0] == 'X' and self.board[1][1] == 'X' and self.board[1][2] == 'X':
+			return True
+		if self.board[2][0] == 'X' and self.board[2][1] == 'X' and self.board[2][2] == 'X':
+			return True
+		if self.board[0][0] == 'X' and self.board[1][0] == 'X' and self.board[2][0] == 'X':
+			return True
+		if self.board[0][1] == 'X' and self.board[1][1] == 'X' and self.board[2][1] == 'X':
+			return True
+		if self.board[0][2] == 'X' and self.board[1][2] == 'X' and self.board[2][2] == 'X':
+			return True
+		if self.board[0][0] == 'X' and self.board[1][1] == 'X' and self.board[2][2] == 'X':
+			return True
+		if self.board[0][2] == 'X' and self.board[1][1] == 'X' and self.board[2][0] == 'X':
+			return True
+		return False
+
+	def getLegalActions(self, playerIndex = 0):
+		actions = []
+		for row in self.board:
+			for item in row:
+				if item != 'X':
+					actions.append(item)
+		return actions 
+
+	def generateSuccessor(self, playerIndex, action):
+		successorGameState = copy.deepcopy(self)
+		successorGameState.placeTo(int(action))
+		return successorGameState
+
+
+class OneBoardMinimaxAgent():
+
+	def __init__(self, depth = 5):
+		self.depth = depth
+
+	def evaluationFunction(self, gameState, depth):
+		if depth % 2 == 0:
+			return -1
+		else:
+			return 1
+	
+	def max_choice(self, gameState, depth):
+		actions = gameState.getLegalActions(0)
+		max_temp = -99999999
+		action_temp = None
+		for action in actions:
+			this_value, suc_action = self.this_node_choice(gameState=gameState.generateSuccessor(0, action), depth=depth+1)
+			if this_value > max_temp:
+				max_temp = this_value
+				action_temp = action
+		return (max_temp, action_temp)
+
+	def min_choice(self, gameState, depth):
+		actions = gameState.getLegalActions(1)
+		min_temp = 99999999
+		action_temp = None
+		for action in actions:
+			this_value, suc_action = self.this_node_choice(gameState=gameState.generateSuccessor(1, action), depth=depth+1)
+			if this_value < min_temp:
+				min_temp = this_value
+				action_temp = action
+		return (min_temp, action_temp)
+
+	def gameOver(self, gameState, depth):
+		if gameState.isLose() or depth == self.depth * 2:
+			return True
+		else:
+			return False
+
+	def this_node_choice(self, gameState, depth): 
+		if self.gameOver(gameState, depth):
+			return (self.evaluationFunction(gameState, depth), None)
+		if depth % 2 == 0:
+			return self.min_choice(gameState, depth)
+		else:
+			return self.max_choice(gameState, depth)
+	
+	def getAction(self, gameState):
+		root_max_value, root_max_action = self.this_node_choice(gameState, 0)
+		return root_max_value
+
+class ReflexAgent():
+	def fingerPrint(self, boardN):
+		def countX(boardN):
+			count = 0
+			for row in boardN:
+				for item in row:
+					if item == 'X':
+						count += 1
+			return count
+
+		def isDead(boardN):
+			if boardN[0][0] == 'X' and boardN[0][1] == 'X' and boardN[0][2] == 'X':
+				return True
+			if boardN[1][0] == 'X' and boardN[1][1] == 'X' and boardN[1][2] == 'X':
+				return True
+			if boardN[2][0] == 'X' and boardN[2][1] == 'X' and boardN[2][2] == 'X':
+				return True
+			if boardN[0][0] == 'X' and boardN[1][0] == 'X' and boardN[2][0] == 'X':
+				return True
+			if boardN[0][1] == 'X' and boardN[1][1] == 'X' and boardN[2][1] == 'X':
+				return True
+			if boardN[0][2] == 'X' and boardN[1][2] == 'X' and boardN[2][2] == 'X':
+				return True
+			if boardN[0][0] == 'X' and boardN[1][1] == 'X' and boardN[2][2] == 'X':
+				return True
+			if boardN[0][2] == 'X' and boardN[1][1] == 'X' and boardN[2][0] == 'X':
+				return True
+			return False
+
+		def isB(boardN):
+			gameState = OneBoardState(boardN)
+			oneBoardMinimaxAgent = OneBoardMinimaxAgent()
+			actions = gameState.getLegalActions()
+			aFlag = False
+			oneFlag = False
+			for action in actions:
+				if oneBoardMinimaxAgent.getAction(gameState.generateSuccessor(0, action)) == 1:
+					aFlag = True
+				if isDead(boardN) or (countX(boardN) == 1 and boardN[1][1] != 'X'):
+					oneFlag = True
+			if aFlag and oneFlag and countX(boardN)>=2:
+				return True
+			return False
+
+		if countX(boardN) == 0:
+			return 'c'
+		if isDead(boardN)\
+			or (countX(boardN) == 1 and boardN[1][1] != 'X')\
+			or (countX(boardN) == 3 and boardN[0][0] == 'X' and boardN[1][2] == 'X' and boardN[2][1] == 'X')\
+			or (countX(boardN) == 3 and boardN[0][2] == 'X' and boardN[1][0] == 'X' and boardN[2][1] == 'X')\
+			or (countX(boardN) == 3 and boardN[2][2] == 'X' and boardN[1][0] == 'X' and boardN[0][1] == 'X')\
+			or (countX(boardN) == 3 and boardN[2][0] == 'X' and boardN[0][1] == 'X' and boardN[1][2] == 'X'):
+				return '1'
+		if countX(boardN) == 1 and boardN[1][1] == 'X':
+			return 'cc'
+		oneBoardState = OneBoardState(boardN)
+		oneBoardMinimaxAgent = OneBoardMinimaxAgent()
+		if oneBoardMinimaxAgent.getAction(oneBoardState) == 1:
+			return 'a'
+		if isB(boardN):
+			return 'b'
+		return 'Nope'
+
+
+	def multiply(self, xs = '1', ys = 'a' , zs = 'b'):
+		def s2num(s):
+			#   1 a b c
+			#-->1 3 5 7
+			dict = {'1':1, 'a':3, 'b':5, 'c':7, 'cc':49, 'bb':25, 'bc':35}
+			if s in dict:
+				return dict[s]
+			else:
+				return 'Nope'
+		def num2s(num):
+			dict = {49:'cc', 3:'a', 25:'bb', 35:'bc'}
+			if num in dict:
+				return dict[num]
+			else:
+				return 'Nope'
+		x = s2num(xs)
+		y = s2num(ys)
+		z = s2num(zs)
+		xyz = num2s(x*y*z)
+		return xyz
+
+	def evaluationFunction(self, currentGameState, action):
+		successorGameState = currentGameState.generateSuccessor(action)
+		fingerPrint_A = self.fingerPrint(successorGameState.board['A'])
+		fingerPrint_B = self.fingerPrint(successorGameState.board['B'])
+		fingerPrint_C = self.fingerPrint(successorGameState.board['C'])
+		multiplyValue = self.multiply(fingerPrint_A, fingerPrint_B, fingerPrint_C)
+		if multiplyValue == 'cc' or multiplyValue == 'a' or multiplyValue == 'bb' or multiplyValue == 'bc':
+			return 999999
+		else:
+			return -999999
+
+	def getAction(self, gameState):
+
+		legalMoves = gameState.getLegalActions()
+
+		scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
+		bestScore = max(scores)
+		bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+		chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+
+		return legalMoves[chosenIndex]
+
+
 
 if __name__ == '__main__':
 	game = GameState()
@@ -119,14 +348,16 @@ if __name__ == '__main__':
 				print 'ERROR INPUT!!'
 				continue
 
-		
+
 		#last move is HM move
 		#if game.playerIndex == 'HM':
 		else: 
 			game.display()
 			game.playerIndex = 'AI'
-			game.aiPlace()
+			reflexAgent = ReflexAgent()
+			print reflexAgent.getAction(game)
 	print game.playerIndex + ' is LOSSER!!'
+
 
 
 
